ac1cdf5a7b35a1dd2a4f77f3fa2714934a614e98
Arimazz <bodyaprymyak@icloud.com>
2020-03-23 02:50:13 +0200

    Updates

---
+++ precache-manifest.8174eec290e874f07f09e484d42a6df8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f0fe71e0e395d0680552df6541cf4f",
+    "url": "/test-task-1/index.html"
+  },
+  {
+    "revision": "0e77087f4f2231a649be",
+    "url": "/test-task-1/static/css/main.b9f9c837.chunk.css"
+  },
+  {
+    "revision": "1d0ef530ed1c9f161f9f",
+    "url": "/test-task-1/static/js/2.911ec7f6.chunk.js"
+  },
+  {
+    "revision": "0e77087f4f2231a649be",
+    "url": "/test-task-1/static/js/main.60eb8f9e.chunk.js"
+  },
+  {
+    "revision": "046462d0bfb6cafe115f",
+    "url": "/test-task-1/static/js/runtime~main.11fbd7ff.js"
+  }
+]);
